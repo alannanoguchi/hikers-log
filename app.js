6636309743d758aefa83f339e5502fe4d928197d
Alanna <alanna.noguchi@students.makeschool.com>
2020-07-13 21:38:31 -0700

    added vanilla boostrap

--- app.js
+++ app.js
@@ -11,10 +11,23 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
-// Render the "home" layout for the main page and send the following msg
+// INDEX
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
-})
+    res.render('hikes-index', { hikes: hikes });
+  })
+
+// OUR MOCK ARRAY OF PROJECTS
+var hikes = [
+    { title: "I am your first hike", desc: "A great event that is super fun to look at and good", imgUrl: "https://images.unsplash.com/photo-1464852045489-bccb7d17fe39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80" },
+    { title: "I am your second hike", desc: "A great event that is super fun to look at and good", imgUrl: "https://images.unsplash.com/photo-1464852045489-bccb7d17fe39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80" },
+    { title: "I am your third hike", desc: "A great event that is super fun to look at and good", imgUrl: "https://images.unsplash.com/photo-1464852045489-bccb7d17fe39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80" }
+  ]
+  
+  // INDEX
+  app.get('/hikes', (req, res) => {
+    res.render('hikes-index', { hikes: hikes });
+  })
+
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
